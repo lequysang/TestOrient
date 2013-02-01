91b9ac178f9280ae6ba9163cfd699de31c6543d2
lequysang <lequysang84@gmail.com>
2013-02-01 13:18:02 +0700

    Initial Commit

---
+++ TestOrient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestOrient
+//
+//  Created by Quy Sang Le on 2/1/13.
+//  Copyright (c) 2013 Quy Sang Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestOrient/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestOrient
+//
+//  Created by Quy Sang Le on 2/1/13.
+//  Copyright (c) 2013 Quy Sang Le. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
